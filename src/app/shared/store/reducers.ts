34c6037222c738ade89d40108f4f292a455ff967
James Ma <k@whiteoranges.com>
2017-08-15 12:12:54 -0400

    Added: store localstorage sync

--- src/app/shared/store/reducers.ts
+++ src/app/shared/store/reducers.ts
@@ -6,7 +6,59 @@ import { combineReducers } from '@ngrx/store';
 /**
  * https://github.com/ngrx/store
  */
+export const local: any = {
+  key: 'app.angular.cli.ts',
+  payload: {
+    auth: { token: 'string' },
+    translate: { language: 'string' },
+  },
+};
+
+export function parse(payload: any, state: any): any {
+
+  const outpt: any = {};
+  const keys: Array<string> = Object.keys(payload);
+  if (!state) {
+    return outpt;
+  }
+
+  keys.forEach((k) => {
+    if (state[ k ]) {
+      outpt[ k ] = ( typeof payload[ k ] === 'object' )
+        ? parse(payload[ k ], state[ k ])
+        : state[ k ]
+      ;
+    }
+  });
+
+  return outpt;
+
+}
+
+export function storage(reducer: ActionReducer<any>): ActionReducer<any> {
+
+  return (state: any, action: any): any => {
+
+    let payload: any;
+    state = reducer(state, action);
+
+    if (action.type === '@ngrx/store/init') {
+      payload = localStorage.getItem(local.key);
+      payload = ( payload ) ? JSON.parse(payload) : state;
+      payload = ( payload ) ? Object.assign(state, payload) : state;
+      return reducer(payload, action);
+    }
+
+    payload = parse(local.payload, state);
+    localStorage.setItem(local.key, JSON.stringify(payload));
+    return state;
+
+  };
+
+}
+
 export const metaReducers: Array<ActionReducer<any>> = [
+  storage,
 ];
 
 export const reducers: ActionReducerMap<any> = {
